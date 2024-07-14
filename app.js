adfa074c9cfba6840338279418997c84b218ad0d
RomanOstrous <ostrous.roman@gmail.com>
2024-07-14 23:28:32 +0300

    Delete & Update Requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -26,6 +27,7 @@ app.listen(PORT, (error) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('styles'));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
@@ -51,6 +53,40 @@ app.get('/posts/:id', (req, res) => {
     .catch(er => {
       console.log(er);
       res.render(createPath('error'), {title: 'Error'})
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  const title = 'Post';
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then(result => res.sendStatus(200))
+    .catch(er => {
+      console.log(er);
+      res.render(createPath('error'), {title: 'Error'})
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
     });
 });
 
